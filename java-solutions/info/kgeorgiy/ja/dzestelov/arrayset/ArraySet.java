972034eaa28470c2b12d8e5004e19be579c0b3e3
Khetag <dzkhetag@gmail.com>
2022-02-20 20:50:43 +0300

    [HM-2] Pre solution #3

--- java-solutions/info/kgeorgiy/ja/dzestelov/arrayset/ArraySet.java
+++ java-solutions/info/kgeorgiy/ja/dzestelov/arrayset/ArraySet.java
@@ -40,28 +40,18 @@ public class ArraySet<E> extends AbstractSet<E> implements NavigableSet<E> {
     }
 
     private int lowerIndex(E t) {
-        //        int i = Collections.binarySearch(elements, Objects.requireNonNull(t), comparator);
-        //        return get(i >= 0 ? i - 1 : -i - 2);
-
         return getIndex(t, -1, -1);
     }
 
     private int floorIndex(E t) {
-        //        int i = Collections.binarySearch(elements, Objects.requireNonNull(t), comparator);
-        //        return get(i >= 0 ? i : -i - 1);
-
         return getIndex(t, 0, -1);
     }
 
     private int ceilingIndex(E t) {
-        //        int i = Collections.binarySearch(elements, Objects.requireNonNull(t), comparator);
-        //        return get(i >= 0 ? i : -i - 1);
         return getIndex(t, 0, 0);
     }
 
     private int higherIndex(E t) {
-        //        int i = Collections.binarySearch(elements, Objects.requireNonNull(t), comparator);
-        //        return get(i >= 0 ? i + 1 : -i - 1);
         return getIndex(t, 1, 0);
     }
 
@@ -120,26 +110,35 @@ public class ArraySet<E> extends AbstractSet<E> implements NavigableSet<E> {
     }
 
     @Override
-    public NavigableSet<E> subSet(E fromElement, boolean fromInclusive, E toElement, boolean toInclusive) {
-        int from = fromInclusive ? ceilingIndex(fromElement) : higherIndex(fromElement);
-        int to = toInclusive ? floorIndex(toElement) : lowerIndex(toElement);
-        return new ArraySet<>(elements.subList(from, to), comparator);
-    }
-
-    @Override
-    public NavigableSet<E> headSet(E toElement, boolean inclusive) {
-        return subSet(first(), true, toElement, inclusive);
-    }
-
-    @Override
-    public NavigableSet<E> tailSet(E fromElement, boolean inclusive) {
-        return subSet(fromElement, inclusive, last(), true);
-    }
-
-    @Override
     public SortedSet<E> subSet(E fromElement, E toElement) {
         return subSet(fromElement, true, toElement, false);
     }
+
+    @Override
+    public NavigableSet<E> subSet(E fromElement, boolean fromInclusive, E toElement, boolean toInclusive) {
+        int from = fromInclusive ? ceilingIndex(fromElement) : higherIndex(fromElement);
+        int to = toInclusive ? floorIndex(toElement) : lowerIndex(toElement);
+
+        if (to < from) {
+            throw new IllegalArgumentException("fromElement > toElement: " + elements.toString() + ", " + comparator.toString() + ": subSet(" + fromElement + ", " + fromInclusive + ", " + toElement + ", " + toInclusive + ")");
+        }
+        return new ArraySet<>(elements.subList(from, to + 1), comparator);
+    }
+//    private boolean inRange(E element, boolean inclusive) {
+//        if (isEmpty()) {
+//            return false;
+//        } else {
+//            if (inclusive) {
+//                if (comparator == null) {
+//                    return first() < element;
+//                }
+//                return comparator.compare(first(), element) <= 0 && comparator.compare(element, last()) <= 0;
+//            } else {
+//                return comparator.compare(first(), element) < 0 && comparator.compare(element, last()) < 0;
+//            }
+//        }
+
+//    }
 
     @Override
     public SortedSet<E> headSet(E toElement) {
@@ -147,8 +146,24 @@ public class ArraySet<E> extends AbstractSet<E> implements NavigableSet<E> {
     }
 
     @Override
+    public NavigableSet<E> headSet(E toElement, boolean inclusive) {
+        if (isEmpty()) {
+            return new ArraySet<>();
+        }
+        return subSet(first(), true, toElement, inclusive);
+    }
+
+    @Override
     public SortedSet<E> tailSet(E fromElement) {
         return tailSet(fromElement, true);
+    }
+
+    @Override
+    public NavigableSet<E> tailSet(E fromElement, boolean inclusive) {
+        if (isEmpty()) {
+            return new ArraySet<>();
+        }
+        return subSet(fromElement, inclusive, last(), true);
     }
 
     @Override
@@ -158,11 +173,19 @@ public class ArraySet<E> extends AbstractSet<E> implements NavigableSet<E> {
 
     @Override
     public E first() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
         return elements.get(0);
     }
 
     @Override
     public E last() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
         return elements.get(elements.size() - 1);
     }
 
