6a7c071e60c87761b380a4c0245266fd3ad8f04b
Khetag <dzkhetag@gmail.com>
2022-03-24 14:19:28 +0300

    [HM-5] Add module

---
+++ java-solutions/module-info.java
@@ -0,0 +1,9 @@
+module info.kgeorgiy.ja.dzestelov {
+    requires java.compiler;
+    requires info.kgeorgiy.java.advanced.implementor;
+    requires info.kgeorgiy.java.advanced.student;
+    requires info.kgeorgiy.java.advanced.arrayset;
+    requires info.kgeorgiy.java.advanced.walk;
+
+    exports info.kgeorgiy.ja.dzestelov.implementor;
+}
