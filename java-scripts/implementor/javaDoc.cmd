3221f42218755192d560f3fb2195e430ff69d21a
Khetag <dzkhetag@gmail.com>
2022-03-24 12:58:19 +0300

    [HM-5] Add jar script

--- java-scripts/implementor/javaDoc.cmd
+++ java-scripts/implementor/javaDoc.cmd
@@ -3,6 +3,6 @@ cd C:\Users\Khetag\Desktop\Java
 
 SET implTest=.\java-advanced-2022\modules\info.kgeorgiy.java.advanced.implementor\info\kgeorgiy\java\advanced\implementor
 SET impl=.\java-advanced\java-solutions\info\kgeorgiy\ja\dzestelov\implementor\Implementor.java
-SET out=C:\Users\Khetag\Desktop\Java\java-advanced\out
+SET out=C:\Users\Khetag\Desktop\Java\java-advanced\out\javadoc
 
 javadoc -private %impl% %implTest%\JarImpler.java %implTest%\Impler.java %implTest%\ImplerException.java -d %out% 
