84249bb4ffe6d800d1612a8f218ed5adf585d9b6
Khetag Dzestelov <dzkhetag@gmail.com>
2022-04-28 11:57:30 +0300

    [HM-8] Add prefinal solution

--- java-solutions/info/kgeorgiy/ja/dzestelov/crawler/WebCrawler.java
+++ java-solutions/info/kgeorgiy/ja/dzestelov/crawler/WebCrawler.java
@@ -11,12 +11,14 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * Class that allows to crawl websites.
  */
 public class WebCrawler implements Crawler {
 
+    private static final int CLOSE_TIMEOUT_SECONDS = 10;
     private final Downloader downloader;
     private final int perHost;
 
@@ -30,8 +32,22 @@ public class WebCrawler implements Crawler {
         this.downloadService = Executors.newFixedThreadPool(downloaders);
         this.extractService = Executors.newFixedThreadPool(extractors);
         this.downloader = downloader;
-        this.downloadCompletionService = new WrappedCompletionService<>(this.downloadService);
-        this.extractCompletionService = new WrappedCompletionService<>(this.extractService);
+        this.downloadCompletionService = new WrappedCompletionService<>(new ExecutorCompletionService<>(this.downloadService));
+        this.extractCompletionService = new WrappedCompletionService<>(new ExecutorCompletionService<>(this.extractService));
+    }
+
+    /**
+     * toDO
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            Downloader downloader = new CachingDownloader(Path.of(""));
+            // toDo
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static String getHost(final String url) throws MalformedURLException {
@@ -53,54 +69,46 @@ public class WebCrawler implements Crawler {
         return index >= 0 ? url.substring(0, index) : url;
     }
 
-    /**
-     * toDO
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
-        try {
-            Downloader downloader = new CachingDownloader(Path.of(""));
-            // toDo
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Downloads web site up to specified depth.
-     *
-     * @param url   start <a href="http://tools.ietf.org/html/rfc3986">URL</a>.
-     * @param depth download depth.
-     * @return download result.
-     */
     @Override
     public Result download(String url, int depth) {
-        List<String> downloaded = new ArrayList<>();
-        ConcurrentMap<String, IOException> errors = new ConcurrentHashMap<>();
+        return downloadVerifyUrls(url, depth, x -> true);
+    }
 
-        Set<String> used = new HashSet<>();
-        Queue<List<String>> current = new ArrayDeque<>();
-        current.add(List.of(url));
+    private Result downloadVerifyUrls(String url, int depth, Predicate<String> urlPredicate) {
+        final List<String> downloaded = new ArrayList<>();
+        final ConcurrentMap<String, IOException> errors = new ConcurrentHashMap<>();
+        final ConcurrentMap<String, Semaphore> hosts = new ConcurrentHashMap<>();
 
-        Consumer<UrlDocument> downed = document -> {
-            downloaded.add(document.getUrl());
-            extractCompletionService.add(() -> getLinks(document, errors));
+        final Set<String> used = new HashSet<>();
+        Queue<String> current = new ArrayDeque<>();
+        Queue<String> next = new ArrayDeque<>();
+        if (urlPredicate.test(url)) {
+            used.add(url);
+            current.add(url);
+        }
+
+        Consumer<List<String>> extracted = list -> {
+            for (String u : list) {
+                if (!used.contains(u) && urlPredicate.test(u)) {
+                    next.add(u);
+                    used.add(u);
+                }
+            }
         };
 
         while (depth-- > 0) {
-            Queue<List<String>> next = new ArrayDeque<>();
             while (!current.isEmpty()) {
-                for (String currentUrl : current.poll()) {
-                    if (!used.contains(currentUrl)) {
-                        used.add(currentUrl);
-                        downloadCompletionService.add(() -> getDocument(currentUrl, errors));
+                String currentUrl = current.remove();
+                Future<UrlDocument> add = downloadCompletionService.add(() -> {
+                    UrlDocument document = getDocument(currentUrl, errors, hosts);
+                    if (document != null) {
+                        downloaded.add(document.getUrl());
                     }
-                }
-
-                downloadCompletionService.pollAll(downed);
-                extractCompletionService.pollAll(next::add);
+                    return document;
+                });
+                extractCompletionService.add(() -> getFutureLinks(add, errors));
             }
+            extractCompletionService.pollAll(extracted);
 
             current = next;
         }
@@ -108,22 +116,40 @@ public class WebCrawler implements Crawler {
         return new Result(downloaded, errors);
     }
 
-    private List<String> getLinks(UrlDocument document, ConcurrentMap<String, IOException> errors) {
+    private List<String> getFutureLinks(Future<UrlDocument> document, ConcurrentMap<String, IOException> errors) {
         try {
+            UrlDocument urlDocument = document.get();
+            if (urlDocument == null) {
+
+            }
             return document.extractLinks();
-        } catch (IOException e) {
-            errors.put(document.getUrl(), e);
+        } catch (Exception e) {
+            putError(document.getUrl(), errors, e);
             return null;
         }
     }
 
-    private UrlDocument getDocument(String url, ConcurrentMap<String, IOException> errors) {
+    private UrlDocument getDocument(String url, ConcurrentMap<String, IOException> errors, ConcurrentMap<String, Semaphore> hosts) {
+        String host = null;
         try {
-            return new UrlDocument(url, downloader.download(url));
-        } catch (IOException e) {
-            errors.put(url, e);
+            host = getHost(url);
+            hosts.putIfAbsent(host, new Semaphore(perHost));
+            hosts.get(host).acquire();
+            UrlDocument document = new UrlDocument(url, downloader.download(url));
+            hosts.get(host).release();
+            return document;
+        } catch (MalformedURLException e) {
+            putError(url, errors, e);
+            return null;
+        } catch (Exception e) {
+            hosts.get(host).release();
+            putError(url, errors, e);
             return null;
         }
+    }
+
+    private void putError(String url, ConcurrentMap<String, IOException> errors, Exception e) {
+        errors.put(url, e instanceof IOException ? (IOException) e : new IOException("Cannot download document", e.getCause()));
     }
 
     /**
@@ -131,36 +157,50 @@ public class WebCrawler implements Crawler {
      */
     @Override
     public void close() {
+        downloadService.shutdown();
+        extractService.shutdown();
 
+        try {
+            waitUntilAllTasksCompleted(downloadService);
+            waitUntilAllTasksCompleted(extractService);
+        } catch (InterruptedException e) {
+            downloadService.shutdownNow();
+            extractService.shutdownNow();
+            e.printStackTrace();
+        }
     }
 
-    static class WrappedCompletionService<T> {
+    private void waitUntilAllTasksCompleted(ExecutorService service) throws InterruptedException {
+        if (!service.awaitTermination(CLOSE_TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
+            service.shutdownNow();
+            System.err.println("[WARNING] WebCrawler terminated before all tasks completed");
+        }
+    }
 
-        private final ExecutorService executorService;
-        private final Queue<Future<T>> queue;
+    class WrappedCompletionService<T> {
+
+        private final CompletionService<T> completionService;
         private int counter = 0;
 
-        WrappedCompletionService(ExecutorService executorService) {
-            this.executorService = executorService;
-            this.queue = new ArrayDeque<>();
+        WrappedCompletionService(CompletionService<T> completionService) {
+            this.completionService = completionService;
         }
 
-        private void add(Callable<T> task) {
+        private Future<T> add(Callable<T> task) {
             counter++;
-            queue.add(executorService.submit(task));
+            return completionService.submit(task);
         }
 
         private T poll() {
             try {
-                T t = queue.remove().get();
+                T t = completionService.take().get();
                 counter--;
                 return t;
-            } catch (ExecutionException e) {
-                System.out.println("exec");
-                return null;
             } catch (InterruptedException e) {
-                System.out.println("interrupted");
+                close();
                 return null;
+            } catch (ExecutionException e) {
+                throw new IllegalStateException("No error expected" + e.getMessage());
             }
         }
 
